cdc076e6d93f830809fd1df11608978ae51df66c
henriquepin <pin.henrique03@yahoo.com>
2016-07-19 10:13:41 -0300

    Atualização

--- src/cap7/PrimeiroArray.java
+++ src/cap7/PrimeiroArray.java
@@ -13,12 +13,12 @@ public class PrimeiroArray {
 		numeros[1] = 200;
 		numeros[2] = 300;
 		
-		System.out.println(numeros[0]);
-		System.out.println(numeros[1]);
-		System.out.println(numeros[2]);
+//		System.out.println(numeros[0]);
+//		System.out.println(numeros[1]);
+//		System.out.println(numeros[2]);
 		
 		
-		String [] palavras = new String[4];
+		String [] palavras = new String[4]; // "declarando um Array"
 		
 		palavras[0] = "Não";
 		palavras[1] = "estou";
@@ -26,10 +26,15 @@ public class PrimeiroArray {
 		palavras[3] = "sono";
 		
 		
-		System.out.println(palavras[0]);
-		System.out.println(palavras[1]);
-		System.out.println(palavras[2]);
-		System.out.println(palavras[3]);
+//		System.out.println(palavras[0]);
+//		System.out.println(palavras[1]);
+//		System.out.println(palavras[2]);
+//		System.out.println(palavras[3]);
 		
+		
+		for(int i = 0; i < palavras.length; i++){
+			System.out.print(palavras[i] + " ");
+		}
 	}
+	
 }
