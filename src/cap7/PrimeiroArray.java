e5bb50714bccb73e7fc6cb7ab4b4dc0b3be8306e
henriquepin <pin.henrique03@yahoo.com>
2016-07-19 10:00:17 -0300

    Atualização

--- src/cap7/PrimeiroArray.java
+++ src/cap7/PrimeiroArray.java
@@ -7,5 +7,29 @@ public class PrimeiroArray {
 		int z = 30;
 		
 		
+		int [] numeros = new int[3];
+		
+		numeros[0] = 100;
+		numeros[1] = 200;
+		numeros[2] = 300;
+		
+		System.out.println(numeros[0]);
+		System.out.println(numeros[1]);
+		System.out.println(numeros[2]);
+		
+		
+		String [] palavras = new String[4];
+		
+		palavras[0] = "Não";
+		palavras[1] = "estou";
+		palavras[2] = "com";
+		palavras[3] = "sono";
+		
+		
+		System.out.println(palavras[0]);
+		System.out.println(palavras[1]);
+		System.out.println(palavras[2]);
+		System.out.println(palavras[3]);
+		
 	}
 }
