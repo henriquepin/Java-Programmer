1b09ffc796534016518cf861ffe46c5debf5bfc2
henriquepin <pin.henrique03@yahoo.com>
2016-07-27 10:39:16 -0300

    27072016 - cap9 Quinta desafio

---
+++ src/cap9/quintodesafio/geral/Endereco.java
@@ -0,0 +1,48 @@
+package cap9.quintodesafio.geral;
+
+public final class Endereco {//classes marcadas como "final" nao podem ser herdadas
+	String logradouro;
+	int nr;
+	String bairro;
+	
+	public Endereco(){}
+	
+	public Endereco(String logradouro, int nr, String bairro) {
+		this.logradouro = logradouro;
+		this.nr = nr;
+		this.bairro = bairro;
+	}
+	
+//	abstract void gerarEndereco(); //classes finais não podem ter metodos abstratos
+	
+	
+	public String getLogradouro() {
+		return logradouro;
+	}
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+	public int getNr() {
+		return nr;
+	}
+	public void setNr(int nr) {
+		this.nr = nr;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	
+	public void setEndereco(String logradouro, int nr, String bairro){
+		setLogradouro(logradouro);
+		setNr(nr);
+		setBairro(bairro);
+	}
+	
+	public String getDados(){
+		return getLogradouro() + " - " + getNr() + " - " + getBairro();
+	}
+	
+}
