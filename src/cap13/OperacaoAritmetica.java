a6f4c34117e2dbcb149396bb5e6bf946f276622c
henriquepin <pin.henrique03@yahoo.com>
2016-08-03 09:54:42 -0300

    03082016 - cap13

--- src/cap13/OperacaoAritmetica.java
+++ src/cap13/OperacaoAritmetica.java
@@ -1,7 +1,11 @@
 package cap13;
+@FunctionalInterface//anotação para checar se uma interface é funcional ou não(indica que é uma interface funcional para o compilador
 
 public interface OperacaoAritmetica {
 
     double execute(double value1, double value2);
+    
+//    double qualquerCoisa(double d);
+    
     
 }
