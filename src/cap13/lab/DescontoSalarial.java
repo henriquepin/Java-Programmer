d0709e8fdce8a1499610e918d5598cc185034dd2
henriquepin <pin.henrique03@yahoo.com>
2016-08-04 10:09:04 -0300

    05082016 - cap 14

--- src/cap13/lab/DescontoSalarial.java
+++ src/cap13/lab/DescontoSalarial.java
@@ -7,16 +7,21 @@ public class DescontoSalarial {
 		
 		double salariosBrutos[] = {1350.00, 4320.15, 8235.25, 2500.55, 1830.00, 850.26, 3614.29, 12500.00};
 		
-		double salariosLiquidos[] = {};
+//		double salariosLiquidos[] = {};
 		
-		salariosLiquidos = DoubleArrayUtils.transformaValores(salariosBrutos, sal -> sal * 0.9);
+		double salariosLiquidos[] = DoubleArrayUtils.transformaValores(salariosBrutos, salario -> salario * 0.9);
 		
 //		for(int i = 0; i < salariosLiquidos.length; i++){
 //			System.out.println(salariosLiquidos[i]);
 //			
 //		}
+		/*for aprimorado*/
+//		for(double e: salariosLiquidos){
+//			System.out.println(e);
+//		}
 		
-		DoubleArrayUtils.processaValores(salariosLiquidos, sal -> System.out.println(sal));
+		
+		DoubleArrayUtils.processaValores(salariosLiquidos, salario -> System.out.println(salario));
 	}
 	
 }
--- src/cap13/lab/MaioresSalarios.java
+++ src/cap13/lab/MaioresSalarios.java
@@ -5,20 +5,24 @@ public class MaioresSalarios {
 		
 		double salariosBrutos[] = {1350.00, 4320.15, 8235.25, 2500.55, 1830.00, 850.26, 3614.29, 12500.00};
 		
-		double salariosTop[] = {};
-		
-		salariosTop = DoubleArrayUtils.filtraValores(salariosBrutos, sal -> {
-			if(sal >= 3000){
-				return true;			
-			}else{
-				return false;
-			}
-		});
-//		for(int i = 0; i < salariosTop.length; i++){
+		double salariosTop[] = DoubleArrayUtils.filtraValores(salariosBrutos, salario -> salario >= 3000);//{
+//			if(sal >= 3000){
+//				return true;			
+//			}else{
+//				return false;
+//			}
+//		});
+////	for(int i = 0; i < salariosTop.length; i++){
 //			System.out.println(salariosTop[i]);
 //		}
 		
-		DoubleArrayUtils.processaValores(salariosTop, sal -> System.out.println(sal));
+		/*for aprimorado*/
+//		for(double e : salariosTop){
+//			System.out.println(e);
+//		}
+		
+		
+		DoubleArrayUtils.processaValores(salariosTop, salario -> System.out.println(salario));
 		
 	}
-}
+}  
