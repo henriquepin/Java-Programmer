437f5ff58ca0de3a8a095d158818e18d30dd4417
henriquepin <pin.henrique03@yahoo.com>
2016-08-05 09:54:27 -0300

    05082016 - cap14

--- src/cap14/ExemploStream.java
+++ src/cap14/ExemploStream.java
@@ -12,12 +12,12 @@ public class ExemploStream {
 		/*Adiciona alguns funcionários na lista */
 		list.add(new Funcionario(2005, "Manuel Silva", "Desenvolvedor", 3580.0));
 		list.add(new Funcionario(3018, "João Batista", "Analista", 4580.0));
-		list.add(new Funcionario(2005, "Pedro Barros", "Advogado", 8580.0));
-		list.add(new Funcionario(2005, "João Paulo", "Desenvolvedor", 3580.0));
-		list.add(new Funcionario(2005, "Carla Moreno", "Analista", 8880.0));
-		list.add(new Funcionario(2005, "José Carlos", "Atendente", 1280.0));
-		list.add(new Funcionario(2005, "Eduardo ALves", "Coordenador", 13580.0));
-		list.add(new Funcionario(2005, "Manuel", "Desenvolvedor", 3580.0));
+		list.add(new Funcionario(2008, "Pedro Barros", "Advogado", 8580.0));
+		list.add(new Funcionario(3025, "João Paulo", "Desenvolvedor", 3580.0));
+		list.add(new Funcionario(2852, "Carla Moreno", "Analista", 8880.0));
+		list.add(new Funcionario(2485, "José Carlos", "Atendente", 1280.0));
+		list.add(new Funcionario(2855, "Eduardo ALves", "Coordenador", 13580.0));
+		list.add(new Funcionario(2525, "Manuel", "Desenvolvedor", 2580.0));
 		
 		/*Executa sucessivas operações com os dados da lista */
 		list.stream()
