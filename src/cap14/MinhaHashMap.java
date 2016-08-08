18dba8629c4f65388b83e6edfaefce7fd01de6d3
henriquepin <pin.henrique03@yahoo.com>
2016-08-08 08:41:32 -0300

    08082016 - cap14

---
+++ src/cap14/MinhaHashMap.java
@@ -0,0 +1,21 @@
+package cap14;
+
+import java.util.HashMap;
+
+public class MinhaHashMap<K, V> extends HashMap<K, V>{
+
+	private static final long serialVersionUID = 1L;
+
+	public void qualquer(){
+		System.out.println("Vai Corinthians...");
+	}
+
+	@Override
+	public String toString() {
+		return "MinhaHashMap [toString()=" + super.toString() + "]";
+	}
+	
+	
+	
+		
+}
