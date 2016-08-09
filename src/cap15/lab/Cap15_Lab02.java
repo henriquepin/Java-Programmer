eac778808037407f64362df44fb1a4d48c59bcfc
henriquepin <pin.henrique03@yahoo.com>
2016-08-09 08:57:49 -0300

    09082016 - Cap 15 lab

--- src/cap15/lab/Cap15_Lab02.java
+++ src/cap15/lab/Cap15_Lab02.java
@@ -1,5 +1,41 @@
 package cap15.lab;
 
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.stream.Stream;
+
+
 public class Cap15_Lab02 {
 
+	final static String PASTA_ORIGEM = "C:\\USERS\\DI3508\\WORKSPACE\\DOCUMENTOS2";	
+	final static String PASTA_BACKUP = "C:\\USERS\\DI3508\\WORKSPACE\\BACKUP";
+	
+	public static void main(String[] args) {
+		
+		Path origem = Paths.get(PASTA_ORIGEM);		
+		Path backup = Paths.get(PASTA_BACKUP);
+		
+		
+		try{
+			if(Files.exists(backup)){
+				System.out.println("Documento " + backup.getFileName() + " já existe");
+			}else{
+				Files.createDirectory(backup);
+				System.out.println("Documento " + backup.getFileName() + " criado com sucesso");				
+			}
+			
+			Stream <Path> streamOrigem = Files.list(origem);
+			
+			streamOrigem.forEach(p -> {
+				try{
+					Files.copy(p, backup.resolve(p.getFileName()));			
+				}catch(IOException e){
+				e.printStackTrace();
+				}
+			});
+			
+		}catch(IOException e){
+			e.printStackTrace();
+		}		
+	}	
 }
